5ba407b5c055e88e95cc6dc76785f5e75ddb9aa4
Ozodbek <olimjanovozodbek32@gmail.com>
2024-02-13 09:34:55 +0500

    test

---
+++ Message.cs
@@ -0,0 +1,84 @@
+﻿using Npgsql;
+namespace ConsoleApp3
+{
+    public class Message
+    {
+        private readonly string connectionString = "Server=127.0.0.1;Port=5432;Database=MyData;User Id=postgres;" +
+            "Password=admin;";
+        public string TableName = "Data1";
+        public void GetAll()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = $"select * from {TableName};";
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+                NpgsqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["Name"]}\n{reader["password"]}\n");
+                }
+            }
+        }
+        public string username;
+        string password;
+        public void Insertion()
+        {
+            check:
+            
+            Console.Write("Username -> ");
+            username = Console.ReadLine();
+            Console.Write("Password -> ");
+            password = Console.ReadLine();
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = $"insert into Data1(name, password) values('{username}', '{password}');";
+                    NpgsqlCommand command = new NpgsqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.Clear();
+                Console.WriteLine("USERNAME UJE MAVJUD AKA\n");
+            goto check;
+            }
+        }
+        public void Insertion2()
+        {
+            string message;
+            Console.Write("Message -> ");
+            message = Console.ReadLine();
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = $"insert into messages(name,msg) values('{username}', '{message}');";
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+                command.ExecuteNonQuery();
+            }
+            Console.WriteLine("Yozildi");
+        }
+        public void GetMsg()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = $"select * from messages;";
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+                NpgsqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["name"]} -> {reader["msg"]}\n");
+                }
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new Message();
+            string[] qatorlar = { "GetAll", "Insertion", "ShowMessages" };
+            int cursor = 0;
+            ConsoleKeyInfo key;
+
+            while (true)
+            {
+                for (int i = 0; i < qatorlar.Length; i++)
+                {
+                    if (i == cursor)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"\n\t[{qatorlar[i]}]");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    else
+                        Console.WriteLine($" {qatorlar[i]}");
+                }
+                key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow && cursor > 0)
+                    cursor--;
+                else if (key.Key == ConsoleKey.DownArrow && cursor < qatorlar.Length - 1)
+                    cursor++;
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    if (cursor == 0)
+                    {
+                        Console.Clear();
+                        message.GetAll();
+                        Console.ReadKey();
+                    }
+                    if (cursor == 1)
+                    {
+                        Console.Clear();
+                        message.Insertion();
+                        message.Insertion2();
+                        Console.ReadKey();
+                    }
+                    if (cursor == 2)
+                    {
+                        Console.Clear();
+                        message.GetMsg();
+                        Console.ReadKey();
+                    }
+                }
+                Console.Clear();
+            }
+
+        }
+    }
+}
